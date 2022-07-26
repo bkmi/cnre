7537367edfa4a1d7a7391c4006e494cb77e5fd57
bkmi <12955549+bkmi@users.noreply.github.com>
2022-07-26 10:59:56 +0200

    updated sbi with a comment about changing the kernels, updated the main script

--- seq/02 fixed potential.py
+++ seq/02 fixed potential.py
@@ -16,6 +16,7 @@ import torch
 import torch.distributions
 from matplotlib.colors import BoundaryNorm
 from matplotlib.ticker import MaxNLocator
+from pyro import distributions as pdist
 from sbi.inference import SNRE_A, SNRE_B, infer, prepare_for_sbi, simulate_for_sbi
 from sbi.inference.posteriors import MCMCPosterior
 from sbi.inference.potentials.likelihood_based_potential import LikelihoodBasedPotential
@@ -28,22 +29,24 @@ from tqdm.notebook import tqdm, trange
 
 import cnre
 
-num_observation = 1
+# num_observation = 1
 
-task = sbibm.get_task("gaussian_linear")
-prior = task.get_prior_dist()
-likelihood = task._get_log_prob_fn(num_observation)
-theta_o = task.get_true_parameters(num_observation)
-x_o = task.get_observation(num_observation)
+# task = sbibm.get_task("gaussian_linear")
+# prior = task.get_prior_dist()
+# # likelihood = task._get_log_prob_fn(num_observation)
+# ref_posterior = task._get_reference_posterior(num_observation)
+# theta_o = task.get_true_parameters(num_observation)
+# x_o = task.get_observation(num_observation)
+# print(type(ref_posterior))
 
 
 class Ratio(torch.nn.Module):
-    def __init__(self, likelihood, d, device):
+    def __init__(self, posterior, d, device):
         super().__init__()
         self.net = torch.nn.Sequential(torch.nn.Linear(10, 10))
         self.map = torch.randn(d, d)
         self.net.to(device)
-        self.likelihood = likelihood
+        self.posterior = posterior
 
     def forward(self, w: list):
         """Inputs to (s)nre classifier must be a list containing raw theta and x."""
@@ -52,7 +55,7 @@ class Ratio(torch.nn.Module):
         assert isinstance(theta, torch.Tensor)
         assert len(theta.shape) == 2
 
-        return self.likelihood(theta)
+        return self.posterior.log_prob(theta)
         return prior.log_prob(theta).unsqueeze(-1)
         # return (theta).pow(2).neg().sum(
         #     dim=-1,
@@ -66,7 +69,7 @@ def create_chain(
     prior,
     # potential_fn: Callable,
     transform: Optional[dict] = None,
-    thin: int = 50,
+    thin: int = 1,
     num_workers: int = 1,
     device: Optional[str] = None,
     x_shape: Optional[tuple] = None,
@@ -86,7 +89,7 @@ def create_chain(
         proposal=starting_theta,
         theta_transform=theta_transform,
         # method="slice_np_vectorized",
-        method="nuts",
+        method="hmc",
         thin=thin,
         warmup_steps=0,
         num_chains=starting_theta.shape[0],
@@ -105,19 +108,30 @@ def main():
     n = 2
     d = 2
 
-    starting_theta = prior.sample((n,))
+    dim = 2
+    prior_loc = 2 * torch.ones(dim)
+    prior_cov = 0.1 * torch.eye(dim)
+    prior = pdist.MultivariateNormal(loc=prior_loc, covariance_matrix=prior_cov)
+    theta_o = torch.zeros((1, dim))
+    x_o = torch.zeros((1, dim))
 
-    ratio_fixed = Ratio(likelihood, d, "cpu")
+    posti_loc = torch.zeros((1, dim)).squeeze()
+    posti_cov = 0.1 * torch.eye(dim)
+    ref_posterior = pdist.MultivariateNormal(loc=posti_loc, covariance_matrix=posti_cov)
+
+    starting_theta = prior.sample((n,)) + torch.tensor([[10, 10], [-10, -10]])
+
+    ratio_fixed = Ratio(ref_posterior, d, "cpu")
 
     p = create_chain(starting_theta, ratio_fixed, prior)
 
     # print(starting_theta)
 
-    samples = p.sample((20,), x=x_o)
+    samples = p.sample((40,), x=x_o)
 
     print(samples)
 
-    # print(torch.isclose(samples, starting_theta.expand(*samples.shape)))
+    print(torch.isclose(samples, starting_theta[:1, ...].expand(*samples.shape)))
 
 
 ## Now I'm having issues with pickling... and in the end I will get a sampler which doesn't do random walk. I should probably just rewrite one.
